9d16e8537283f33dc303a3c9038389e8fc7ca22a
Edd1eware <mtw.eduardo.me@gmail.com>
2026-04-28 16:34:40 -0600

    CLEANPOINT MARCA LAS LINEAS A LAS 9:30 Y UN LABEL DE NO TRADE

--- EddiewareOpeningRangeSetup/EddiewareOpeningRangeSetup/obj/Debug/net10.0/EddiewareOpeningRangeSetup.AssemblyInfo.cs
+++ EddiewareOpeningRangeSetup/EddiewareOpeningRangeSetup/obj/Debug/net10.0/EddiewareOpeningRangeSetup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EddiewareOpeningRangeSetup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2269db1eaee492f2b9043bf80f8a12a475bb32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7925dd29243b0d51d86eb14acf72ffe7b0f8ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EddiewareOpeningRangeSetup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EddiewareOpeningRangeSetup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
