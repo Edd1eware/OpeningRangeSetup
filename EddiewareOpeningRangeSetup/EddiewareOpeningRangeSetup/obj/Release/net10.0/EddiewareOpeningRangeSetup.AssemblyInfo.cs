00d688acfd95678842549f653855867729836f98
Edd1eware <mtw.eduardo.me@gmail.com>
2026-07-18 08:23:14 -0600

    avance CLV_At_Entry

--- EddiewareOpeningRangeSetup/EddiewareOpeningRangeSetup/obj/Release/net10.0/EddiewareOpeningRangeSetup.AssemblyInfo.cs
+++ EddiewareOpeningRangeSetup/EddiewareOpeningRangeSetup/obj/Release/net10.0/EddiewareOpeningRangeSetup.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EddiewareOpeningRangeSetup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7295d35f35352815853e88ba2f772e8ce870ba5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e051ca4d73bfbc33edaf6154c3ba796973761f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EddiewareOpeningRangeSetup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EddiewareOpeningRangeSetup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
